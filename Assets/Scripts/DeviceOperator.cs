d04bcb8c8e99b4e40c45403757b3b0a8e191666c
AGabtni <agabt077@gmail.com>
2019-07-16 22:53:02 -0400

    First commit

---
+++ Assets/Scripts/DeviceOperator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceOperator : MonoBehaviour
+{
+    public float radius = 1.5f;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            Debug.Log("Fire 3");
+
+            Collider[] hitColliders =
+            Physics.OverlapSphere(transform.position, radius);
+            foreach (Collider hitCollider in hitColliders)
+            {
+
+
+                if(hitCollider.name == "Door") {
+                    Debug.Log(hitCollider.transform.parent.parent.name);
+
+                    hitCollider.transform.parent.parent.SendMessage("OnPlayerEntered",
+                    SendMessageOptions.DontRequireReceiver);
+                }   
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public Maze mazePrefab;
+
+	public PlayerMovement playerPrefab;
+
+	private Maze mazeInstance;
+
+	private PlayerMovement playerInstance;
+
+	private void Start () {
+
+        InstantMaze();
+    }
+	
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.P)) {
+			RestartGame();
+		}
+	}
+
+    private void InstantMaze()
+    {
+        Camera.main.clearFlags = CameraClearFlags.Skybox;
+        Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        mazeInstance.InstantGenerate();
+        playerInstance = Instantiate(playerPrefab) as PlayerMovement;
+        playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
+        Camera.main.clearFlags = CameraClearFlags.Depth;
+        Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
+
+    }
+
+	private IEnumerator BeginGame () {
+		Camera.main.clearFlags = CameraClearFlags.Skybox;
+		Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
+		mazeInstance = Instantiate(mazePrefab) as Maze;
+		yield return StartCoroutine(mazeInstance.Generate());
+		//playerInstance = Instantiate(playerPrefab) as Player;
+		//playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
+		Camera.main.clearFlags = CameraClearFlags.Depth;
+		Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
+	}
+
+
+	private void RestartGame () {
+		StopAllCoroutines();
+		Destroy(mazeInstance.gameObject);
+		if (playerInstance != null) {
+			Destroy(playerInstance.gameObject);
+		}
+        //StartCoroutine(BeginGame());
+        InstantMaze();
+	}
+}
